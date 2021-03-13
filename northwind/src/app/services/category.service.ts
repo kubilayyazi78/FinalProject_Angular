b76ca7122aed05ee1c652138f5dec459b25bfd72
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-03-13 22:12:07 +0300

    Category eklendi.

--- northwind/src/app/services/category.service.ts
+++ northwind/src/app/services/category.service.ts
@@ -1,16 +1,16 @@
+import { Category } from './../models/category';
 import { ListResponseModel } from './../models/listResponseModel';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Product } from '../models/product';
 @Injectable({
   providedIn: 'root',
 })
-export class ProductService {
-  apiUrl = 'https://localhost:44378/api/products/getall';
+export class CategoryService {
+  apiUrl = 'https://localhost:44378/api/categories/getall';
   constructor(private HttpClient: HttpClient) {}
 
-  getProducts():Observable<ListResponseModel<Product>> {
-   return this.HttpClient.get<ListResponseModel<Product>>(this.apiUrl);
+  getCategories():Observable<ListResponseModel<Category>> {
+   return this.HttpClient.get<ListResponseModel<Category>>(this.apiUrl);
   }
 }
