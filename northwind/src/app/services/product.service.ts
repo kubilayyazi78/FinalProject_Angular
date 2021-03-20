01a126435f4685e56f75f2055b6bb42c6c95dfe1
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-03-21 00:22:32 +0300

    Product post işlemi tamamlandı.

--- northwind/src/app/services/product.service.ts
+++ northwind/src/app/services/product.service.ts
@@ -1,22 +1,26 @@
+import { ResponseModel } from './../models/responseModel';
+import { Product } from './../models/product';
 import { ListResponseModel } from './../models/listResponseModel';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Product } from '../models/product';
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
   apiUrl = 'https://localhost:44378/api/';
 
-  constructor(private HttpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) {}
 
   getProducts():Observable<ListResponseModel<Product>> {
     let newPath=this.apiUrl+"products/getall";
-   return this.HttpClient.get<ListResponseModel<Product>>(newPath);
+   return this.httpClient.get<ListResponseModel<Product>>(newPath);
   }
   getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>> {
     let newPath=this.apiUrl+"products/getbycategory?categoryId="+categoryId;
-    return this.HttpClient.get<ListResponseModel<Product>>(newPath);
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
+   }
+   add(product:Product):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"products/add",product);
    }
 }
