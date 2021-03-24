fe315e8ef3a3de665c08272759c16c9b14d0ed23
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-03-24 23:05:05 +0300

    Token işlemlerine başlandı.

--- northwind/src/app/services/auth.service.ts
+++ northwind/src/app/services/auth.service.ts
@@ -1,15 +1,26 @@
+import { SingleResponseModel } from './../models/singleResponseModel';
+import { TokenModel } from './../models/tokenModel';
+import { ResponseModel } from './../models/responseModel';
 import { HttpClient } from '@angular/common/http';
 import { LoginModel } from './../models/loginModel';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
   apiUrl = 'https://localhost:44378/api/auth/';
-  constructor(private httpClient:HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
-  login(loginModel:LoginModel){
-    return this.httpClient.post(this.apiUrl+"login",loginModel);
+  login(loginModel: LoginModel){
+    return this.httpClient.post<SingleResponseModel<TokenModel>>(this.apiUrl + 'login', loginModel);
+  }
+
+  isAuthenticated() {
+    if (localStorage.getItem('token')) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
